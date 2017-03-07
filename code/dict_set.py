77a4d767ff4d88974e165f86ab10ab0e2a8fdac5
jshn232 <jshn232@163.com>
2017-03-08 00:04:36 +0800

    update

--- code/dict_set.py
+++ code/dict_set.py
@@ -2,3 +2,10 @@
 dict_d = {'port1':8990, 'port2':8991,'port3':8992}
 
 print('port1:%s' % dict_d['port1'])
+
+print('get \'port1\' is %d' % dict_d.get('port1'))
+
+print('get \'port5\' is %d' % dict_d.get('port5',-1))
+
+print('%d' % ('port4' in dict_d))
+print('%d' % ('port1' in dict_d))
