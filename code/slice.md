8be267e7b9e1a8b4c95bdf08e466dbe6766ea304
jshn232 <jshn232@163.com>
2017-03-23 22:54:51 +0800

    update

---
+++ code/slice.md
@@ -0,0 +1,7 @@
+#slice
+##切片操作符
+
+L = ['slice1','slice2','slice3','slice4','slice5']
+
+print(L[0:3])
+print(L[2:4])
---
+++ code/slice.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+L = ['slice1','slice2','slice3','slice4','slice5']
+
+print(L[0:3])
+print(L[2:4])
