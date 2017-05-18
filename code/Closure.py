7f21b9ab27fa9766591a30a0994f8807c282051a
jshn232 <jshn232@163.com>
2017-05-18 16:10:27 +0800

    update

--- code/Closure.py
+++ code/Closure.py
@@ -38,3 +38,27 @@ def fun_dict(**d):
 fun_tuple(1,2,4,5,6)
 
 fun_dict(port_1=8989,port_2=8990,port_3=8991)
+
+
+def count():
+    fs = []
+    for i in range(1,4):
+        def f():
+            return i*i
+        fs.append(f)
+    return fs
+
+print(count())
+f1,f2,f3 = count()
+print(f1())
+print(f2())
+print(f3())
+
+
+def count_2():
+    fs = []
+    for i in range(1,4):
+        fs.append(i*i)
+    return fs
+
+print(count_2())
