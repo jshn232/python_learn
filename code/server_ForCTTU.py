572d05acef72488f6e65cdb7d3f0936eae3c32af
jshn232 <jshn232@163.com>
2018-01-17 21:07:08 +0800

    update

--- code/server_ForCTTU.py
+++ code/server_ForCTTU.py
@@ -2,16 +2,16 @@
 
 import socket,threading,time,datetime,select
 
-lt_socket = []
-lt_port = []
-lt_time_wait = [10,60,10,10,10]
+lt_socket = []   #socket
+lt_port = []     #port
+lt_time_wait = [10,60,10,10,10]   #等待时间
 
-addr_local = '127.0.0.1'
+addr_local = '127.0.0.1'   #localhost
 
-Max_Socket_Num = 5
-Define_Port = 6000
+Max_Socket_Num = 5    #可监听数
+Define_Port = 6000    #监听端口  6000开始
 
-
+#socket 初始化
 for i in range(Max_Socket_Num):
     s_Tmp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     lt_socket.append(s_Tmp)
@@ -20,16 +20,20 @@ for i in range(Max_Socket_Num):
     print('socket_%s ip:%s,port:%s'%(i,addr_local,lt_port[i]))
     print('socket time wait is %ss'%lt_time_wait[i])
 
+
+#写文件函数
 def writefile(buf,no):
-    with open('/Users/jshn/Desktop/%s'%(Define_Port+no),'a') as f:
+    with open('/Users/jshn/Desktop/%s'%(Define_Port+no),'a') as f:        #目录根据实际情况填写
         f.write(time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime()))
         f.write(buf+'\n\n')
 
+
+#主应用
 def tcplink(sock,no):
-    sock.listen(1)
+    sock.listen(1)   #监听
     print('listen port %s'%(Define_Port+no))
     try:
-        s, add = sock.accept()
+        s, add = sock.accept()   #接受
     except :
         print('accept Error %s'%no)
         s.close()
@@ -37,21 +41,21 @@ def tcplink(sock,no):
     print('accept port %s'%(Define_Port+no))
     writefile('accept,Wait %ss' % lt_time_wait[no], no)
     #oldtime = datetime.datetime.now()
-    s.setblocking(0)
+    s.setblocking(0)    #非阻塞   默认的recv()是阻塞的   无法做超时判断
 
     while True:
         try:
-            ready = select.select([s], [], [], lt_time_wait[no])
+            ready = select.select([s], [], [], lt_time_wait[no])    #如果无数据情况超过lt_time_wait秒以后或者有数据
             if ready[0]:
-                date = s.recv(256)
+                date = s.recv(256)  #接收数据
             else:
-                writefile('No Recv Date in %ss'%lt_time_wait[no], no)
+                writefile('No Recv Date in %ss'%lt_time_wait[no], no)   #无数据的情况
                 continue
-            if not date:
+            if not date:   #Client断开连接
                 s.close()
                 print('socket close %s'%(Define_Port+no))
                 writefile('Link cut by client', no)
-                s, add = sock.accept()
+                s, add = sock.accept()    #重新等待Client
                 print('accept port-2 %s' % (Define_Port + no))
                 continue
             else:
@@ -62,11 +66,13 @@ def tcplink(sock,no):
                 #oldtime = newtime
                 pass
         except :
-            print('recv Error %s'%no)
+            print('recv Error %s'%no)   #其他错误
             s.close()
             return
     return
 
+
+#根据不同的socket 创建线程
 for i in range(Max_Socket_Num):
     t = threading.Thread(target=tcplink, args=(lt_socket[i],i))
     t.start()
